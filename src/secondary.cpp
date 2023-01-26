24fda4b30553fea6794384c867d0888a566ed40a
Daphnee Chabal <daphneechabal@fralwb-self-145-109-118-186.wireless.uva.nl>
2023-01-26 12:04:27 +0100

    corrected paths in secondary.cpp

--- src/secondary.cpp
+++ src/secondary.cpp
@@ -801,8 +801,7 @@ void loadData(string net, string dataset)
 			TRAINING_DATA_SIZE = 8;
 			TEST_DATA_SIZE = 8;	
 		}
-		else
-			assert(false && "Only AlexNet and VGG16 supported on CIFAR10");
+
 	}
 	else if (dataset.compare("ImageNet") == 0)
 	{
@@ -1308,7 +1307,89 @@ void selectNetwork(string network, string dataset, string security, NeuralNetCon
 	else if (network.compare("VGG16") == 0)
 	{
 		if(dataset.compare("MNIST") == 0)
-			assert(false && "No VGG16 on MNIST");
+	{
+			NUM_LAYERS = 37;
+			WITH_NORMALIZATION = false;
+			CNNConfig* l0 = new CNNConfig(32,32,3,64,3,1,1,MINI_BATCH_SIZE);
+			ReLUConfig* l1 = new ReLUConfig(32*32*64,MINI_BATCH_SIZE);		
+			CNNConfig* l2 = new CNNConfig(32,32,64,64,3,1,1,MINI_BATCH_SIZE);
+			MaxpoolConfig* l3 = new MaxpoolConfig(32,32,64,2,2,MINI_BATCH_SIZE);
+			ReLUConfig* l4 = new ReLUConfig(16*16*64,MINI_BATCH_SIZE);
+
+			CNNConfig* l5 = new CNNConfig(16,16,64,128,3,1,1,MINI_BATCH_SIZE);
+			ReLUConfig* l6 = new ReLUConfig(16*16*128,MINI_BATCH_SIZE);
+			CNNConfig* l7 = new CNNConfig(16,16,128,128,3,1,1,MINI_BATCH_SIZE);
+			MaxpoolConfig* l8 = new MaxpoolConfig(16,16,128,2,2,MINI_BATCH_SIZE);
+			ReLUConfig* l9 = new ReLUConfig(8*8*128,MINI_BATCH_SIZE);
+
+			CNNConfig* l10 = new CNNConfig(8,8,128,256,3,1,1,MINI_BATCH_SIZE);
+			ReLUConfig* l11 = new ReLUConfig(8*8*256,MINI_BATCH_SIZE);
+			CNNConfig* l12 = new CNNConfig(8,8,256,256,3,1,1,MINI_BATCH_SIZE);
+			ReLUConfig* l13 = new ReLUConfig(8*8*256,MINI_BATCH_SIZE);
+			CNNConfig* l14 = new CNNConfig(8,8,256,256,3,1,1,MINI_BATCH_SIZE);
+			MaxpoolConfig* l15 = new MaxpoolConfig(8,8,256,2,2,MINI_BATCH_SIZE);
+			ReLUConfig* l16 = new ReLUConfig(4*4*256,MINI_BATCH_SIZE);
+
+			CNNConfig* l17 = new CNNConfig(4,4,256,512,3,1,1,MINI_BATCH_SIZE);
+			ReLUConfig* l18 = new ReLUConfig(4*4*512,MINI_BATCH_SIZE);
+			CNNConfig* l19 = new CNNConfig(4,4,512,512,3,1,1,MINI_BATCH_SIZE);
+			ReLUConfig* l20 = new ReLUConfig(4*4*512,MINI_BATCH_SIZE);
+			CNNConfig* l21 = new CNNConfig(4,4,512,512,3,1,1,MINI_BATCH_SIZE);
+			MaxpoolConfig* l22 = new MaxpoolConfig(4,4,512,2,2,MINI_BATCH_SIZE);
+			ReLUConfig* l23 = new ReLUConfig(2*2*512,MINI_BATCH_SIZE);
+
+			CNNConfig* l24 = new CNNConfig(2,2,512,512,3,1,1,MINI_BATCH_SIZE);
+			ReLUConfig* l25 = new ReLUConfig(2*2*512,MINI_BATCH_SIZE);
+			CNNConfig* l26 = new CNNConfig(2,2,512,512,3,1,1,MINI_BATCH_SIZE);
+			ReLUConfig* l27 = new ReLUConfig(2*2*512,MINI_BATCH_SIZE);
+			CNNConfig* l28 = new CNNConfig(2,2,512,512,3,1,1,MINI_BATCH_SIZE);
+			MaxpoolConfig* l29 = new MaxpoolConfig(2,2,512,2,2,MINI_BATCH_SIZE);
+			ReLUConfig* l30 = new ReLUConfig(1*1*512,MINI_BATCH_SIZE);
+
+			FCConfig* l31 = new FCConfig(1*1*512,MINI_BATCH_SIZE,4096);
+			ReLUConfig* l32 = new ReLUConfig(4096,MINI_BATCH_SIZE);
+			FCConfig* l33 = new FCConfig(4096, MINI_BATCH_SIZE, 4096);
+			ReLUConfig* l34 = new ReLUConfig(4096, MINI_BATCH_SIZE);
+			FCConfig* l35 = new FCConfig(4096, MINI_BATCH_SIZE, 1000);
+			ReLUConfig* l36 = new ReLUConfig(1000, MINI_BATCH_SIZE);
+			config->addLayer(l0);
+			config->addLayer(l1);
+			config->addLayer(l2);
+			config->addLayer(l3);
+			config->addLayer(l4);
+			config->addLayer(l5);
+			config->addLayer(l6);
+			config->addLayer(l7);
+			config->addLayer(l8);
+			config->addLayer(l9);
+			config->addLayer(l10);
+			config->addLayer(l11);
+			config->addLayer(l12);
+			config->addLayer(l13);
+			config->addLayer(l14);
+			config->addLayer(l15);
+			config->addLayer(l16);
+			config->addLayer(l17);
+			config->addLayer(l18);
+			config->addLayer(l19);
+			config->addLayer(l20);
+			config->addLayer(l21);
+			config->addLayer(l22);
+			config->addLayer(l23);
+			config->addLayer(l24);
+			config->addLayer(l25);
+			config->addLayer(l26);
+			config->addLayer(l27);
+			config->addLayer(l28);
+			config->addLayer(l29);
+			config->addLayer(l30);
+			config->addLayer(l31);
+			config->addLayer(l32);
+			config->addLayer(l33);
+			config->addLayer(l34);
+			config->addLayer(l35);
+			config->addLayer(l36);
+		}
 		else if (dataset.compare("CIFAR10") == 0)
 		{
 			NUM_LAYERS = 37;
